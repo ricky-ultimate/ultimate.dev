bfa296cc31c835defbdd87fa982724e116b14acd
Ricky Ultimate <rickyogigi@gmail.com>
2024-09-02 00:45:01 +0100

    refactor: minor style update

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -2,12 +2,12 @@ import React from 'react';
 
 const AboutMe = () => {
   return (
-    <div className="prose">
-      <h1>About Me</h1>
+    <section>
+      <h1 className="font-semibold text-2xl mb-8 tracking-tighter">About Me</h1>
       <p>
         This is where you'll find out about me.
       </p>
-    </div>
+    </section>
   );
 };
 
