5b5b2d5489d187331c33c0d9f2ad3fb3cc300068
Ricky Ultimate <rickyogigi@gmail.com>
2024-09-03 22:13:31 +0100

    refactor: remove unnecessary Head tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,6 @@ import { Analytics } from '@vercel/analytics/react';
 import { SpeedInsights } from '@vercel/speed-insights/next';
 import Footer from './components/footer';
 import { baseUrl } from './sitemap';
-import Head from 'next/head';
 
 export const metadata: Metadata = {
   metadataBase: new URL(baseUrl),
@@ -53,13 +52,6 @@ export default function RootLayout({
         GeistMono.variable
       )}
     >
-      <Head>
-        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
-        <link rel="manifest" href="/site.webmanifest" />
-        <title>リッキー</title>
-      </Head>
       <body className="antialiased max-w-xl mx-4 mt-8 lg:mx-auto">
         <main className="flex-auto min-w-0 mt-6 flex flex-col px-2 md:px-0">
           <Navbar />
