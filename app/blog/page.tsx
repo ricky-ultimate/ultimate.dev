61de0772c9a6ea885ff21b619295a4f34f3876aa
Ricky Ultimate <rickyogigi@gmail.com>
2024-09-04 00:02:45 +0100

    refactor: remove unnecessary metdata from blog page

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,10 +1,5 @@
 import { BlogPosts } from 'app/components/posts'
 
-export const metadata = {
-  title: 'Blog',
-  description: 'Read my blog.',
-}
-
 export default function Page() {
   return (
     <section>
