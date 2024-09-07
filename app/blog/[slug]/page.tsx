c1ad07f544534deae78c4a400cf1a03e90341a34
Ricky Ultimate <rickyogigi@gmail.com>
2024-09-07 02:46:52 +0100

    Chore: Invalid name

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -77,7 +77,7 @@ export default function Blog({ params }) {
             url: `${baseUrl}/blog/${post.slug}`,
             author: {
               '@type': 'Person',
-              name: 'My Portfolio',
+              name: 'リッキー',
             },
           }),
         }}
