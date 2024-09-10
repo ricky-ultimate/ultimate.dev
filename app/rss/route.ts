f8debf8a79ab12c9c988c673180ed4fed13fd28f
リッキー <84411143+ricky-ultimate@users.noreply.github.com>
2024-09-10 21:53:04 +0100

    chore: some minor text changes

--- app/rss/route.ts
+++ app/rss/route.ts
@@ -27,9 +27,9 @@ export async function GET() {
   const rssFeed = `<?xml version="1.0" encoding="UTF-8" ?>
   <rss version="2.0">
     <channel>
-        <title>My Portfolio</title>
+        <title>リッキー</title>
         <link>${baseUrl}</link>
-        <description>This is my portfolio RSS feed</description>
+        <description>リッキー's RSS feed</description>
         ${itemsXml}
     </channel>
   </rss>`
